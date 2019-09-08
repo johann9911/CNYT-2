128ee04bf8ba395c150ae87f6e8c96e84c6f26f3
Alejandra Izquierdo <49255267+Alizeci@users.noreply.github.com>
2019-09-08 16:46:51 -0500

    Add files via upload

--- calculadoraC.py
+++ calculadoraC.py
@@ -11,6 +11,54 @@ def inversa(a,b):
     a, b = conjugadoC(a, b)
     return (a, b)
 
+def sumaCV(V):
+    suma = (0, 0)
+    for vc in V:
+        suma = sumaC(suma[0], suma[1], vc[0], vc[1])
+    return suma
+
+def isValidMM(M,M1):
+    valid = True
+    if len(M) != len(M1):
+        valid = False
+    else:
+        for fil in range(len(M)):
+            if len(M[fil]) != len(M1[fil]):
+                valid = False
+    return valid
+
+def isValidMV(M,V):
+    valid = True
+    for fil in range(len(M)):
+        if len(M[fil]) != len(V):
+            valid = False
+    return valid
+
+def productoMM(M,M2):
+    res = []
+    if isValidMM(M,M2) == False:
+        raise ValueError('El número de columnas de M debe ser igual al número de filas de M2')
+    else:
+        M2 = traspuestaM(M2)
+        for fil in range(len(M)):
+            subres = []
+            for fil2 in range(len(M2)):
+                V = productoVV(M[fil2],M2[fil])
+                subres.append(sumaCV(V))
+            res.append(subres)
+        res = traspuestaM(res)
+    return res
+
+def productoVV(V,V2):
+
+    if len(V) != len(V2):
+        raise ValueError('V y V2 deben tener la misma dimension')
+    else:
+        res=[]
+        for fil in range(len(V2)):
+            res.append(productoC(V[fil][0],V[fil][1],V2[fil][0],V2[fil][1]))
+
+        return res
 
 #Funciones principales
 def sumaC(a,b,c,d):
@@ -47,7 +95,7 @@ def polarCartesianoC(r, θ):
         else:
             x = r * math.cos(θ * gradosPC)
             y = r * math.sin(θ * gradosPC)
-        return round(x,2),round(y,2)
+        return x, y
 
 def cartesianoPolarC(x, y):
     r = math.sqrt((x ** 2 + y ** 2))
@@ -65,7 +113,7 @@ def cartesianoPolarC(x, y):
         else:
             k = 1
         θ = arcT + 180 * k
-    return round(r, 2),round(θ, 1)
+    return r, θ
 
 def moduloC(a, b):
     z = math.sqrt(a**2 + b**2)
@@ -98,7 +146,7 @@ def faseC(x, y):
 def sumaV(u,v):
     res = [] #Contenedor de la respuesta
     if len(u) != len(v):
-        raise ValueError('A y B deben tener el mismo tamano')
+        raise ValueError('u y v deben tener el mismo tamano')
     else:
         for fil in range(len(u)):
            res.append(sumaC(u[fil][0],u[fil][1], v[fil][0], v[fil][1]))
@@ -112,23 +160,56 @@ def inversaV(V):
         res.append(inversa(a,b))
     return res
 
-def productoV(r,V):
+def productoCV(r,V):
     res = []  # Contenedor de la respuesta
     for v in range(len(V)):
-        res.append(productoC(r[0],r[1],V[v][0], V[v][1]))
+        res.append(productoC(r[0],r[1],V[v][0],V[v][1]))
     return res
+
+def normaV(V):
+    suma = 0
+    for v in range(len(V)):
+        suma+= (V[v][0])**2 #Real
+        suma+= (V[v][1])**2 #Imaginario
+
+    res = math.sqrt(suma)
+    return res
+
+def distanciaV(V,V2):
+    a, b = 0, 0  #inicialzamos suma de partes de los complejos
+
+    if len(V)!= len(V2):
+        raise ValueError('V y V2 deben tener el mismo tamano')
+    else:
+        for fil in range(len(V)):
+            a+= (V[fil][0] - V2[fil][0])**2
+            b+=  (V[fil][1] - V2[fil][1])**2
+        res = math.sqrt(a+b)
+    return res
+
+def productoInternoVV(V,V2):
+    if len(V) != len(V2):
+        raise ValueError('V y V2 deben tener la misma dimension')
+    else:
+        a, b = 0, 0
+        for fil in range(len(V2)):
+            cV = conjugadoC(V[fil][0],V[fil][1])
+            res = productoC(cV[0],cV[1],V2[fil][0],V2[fil][1])
+            a+= res[0]
+            b+= res[1]
+        return (a, b)
 
 #Matrices
 
-def sumaM(A,B): #Matrices deben tener el mismo tamano
+def sumaM(M,M1): #Matrices deben tener el mismo tamano
     res = [] # Contenedor de la respuesta
-    if len(A) != len(B) or len(A[0]) != len(B[0]):
-        raise ValueError('A y B deben tener la misma dimension')
+    if isValidMM(M,M1) == False:
+        raise ValueError('M y M1 deben tener la misma dimension')
     else:
-        for fil in range(len(A)):
+        for fil in range(len(M)):
             subres = [] #Sub contenedor
-            for col in range (len(A[0])):
-                subres.append(sumaC(A[fil][col][0], A[fil][col][1], B[fil][col][0], B[fil][col][1]))
+            for col in range (len(M[0])):
+                subres.append(sumaC(M[fil][col][0], M[fil][col][1], M1[fil][col][0], M1[fil][col][1]))
             res.append(subres)
     return res
 
@@ -141,7 +222,7 @@ def inversaM(M):
         res.append(subres)
     return res
 
-def productoM(c, M):
+def productoCM(c, M):
     res = []  # Contenedor de la respuesta
     for fil in range(len(M)):
         subres = []  # Sub contenedor
@@ -152,22 +233,56 @@ def productoM(c, M):
 
 def traspuestaM(M):
     res = []  # Contenedor de la respuesta
-    for i in range(len(M[0])):
+    for fil in range(len(M[0])):
         subres = []
-        for j in range(len(M)):
-            subres.append(M[j][i])
+        for col in range(len(M)):
+            subres.append(M[col][fil])
         res.append(subres)
     return  res
 
 def conjugadaM(M):
     res = []  # Contenedor de la respuesta
-    for i in range(len(M[0])):
+    for fil in range(len(M)):
         subres = []
-        for j in range(len(M)):
-            subres.append(conjugadoC(M[i][j][0], M[i][j][1]))
+        for col in range(len(M[0])):
+            subres.append(conjugadoC(M[fil][col][0], M[fil][col][1]))
         res.append(subres)
     return  res
 
 def adjuntaM(M):
-
     return traspuestaM(conjugadaM(M))
+
+def accionMV(M,V):
+    if isValidMV(M,V) == False:
+        raise ValueError('Las columnas de M y V deben tener la misma dimension')
+    else:
+        res = []
+        for fil in range(len(M)):
+            for col in range(len(M[0])):
+                MV = productoVV(M[fil],V)
+                suma = sumaCV(MV)
+            res.append(suma)
+    return res
+
+def isUnitaria(M):
+    valido = True
+    res = productoMM(M,adjuntaM(M))
+    for fil in range(len(res)):
+        for col in range(len(res[0])):
+            if fil == col and valido == True:
+                if res[fil][fil] != (1,0):
+                    valido = False
+            else:
+                if valido == True:
+                    if res[fil][col] != (0, 0):
+                        valido = False
+    return valido
+
+def isHermitiana(M):
+    valido = True
+    res = adjuntaM(M)
+    for fil in range(len(M)):
+        for col in range(len(M[0])):
+            if res[fil][col] != M[fil][col] and valido == True:
+                    valido = False
+    return valido
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import unittest
-from T1 import calculadoraC as ca
-
+import math
+import calculadoraC as ca
 
 class TestCal(unittest.TestCase):
 
@@ -24,12 +24,12 @@ class TestCal(unittest.TestCase):
             ca.divisionC(1,2,0,0)
 
     def test_polarCartesianoC(self):
-        self.assertEqual(ca.polarCartesianoC(3, 60), (1.5, 2.6))
-        self.assertEqual(ca.polarCartesianoC(5, 180), (-5.0, 0.0))
+        self.assertEqual(ca.polarCartesianoC(3, 60), (1.5000000000000004, 2.598076211353316))
+        self.assertEqual(ca.polarCartesianoC(5, 180), (-5.0, 6.123233995736766e-16))
         self.assertEqual(ca.polarCartesianoC(0, 30), 0)
 
     def test_cartesianoPolarC(self):
-        self.assertEqual(ca.cartesianoPolarC(1, 1), (1.41, 45.0))
+        self.assertEqual(ca.cartesianoPolarC(1, 1), (1.4142135623730951, 45.0))
         self.assertEqual(ca.cartesianoPolarC(-3, 0), (3, 180.0))
 
     def test_moduloC(self):
@@ -48,41 +48,79 @@ class TestCal(unittest.TestCase):
     def test_sumaV(self):
         self.assertEqual(ca.sumaV([(1,2),(0,-2),(0,-3)],[(2,3),(1,-1),(0,-4)]), [(3, 5), (1, -3), (0, -7)])
         self.assertEqual(ca.sumaV([(1,0),(0,-1),(1,-1)],[(2,-2),(1,-1),(4,-4)]), [(3, -2), (1, -2), (5, -5)])
+        self.assertEqual(ca.sumaV([(8,3),(-1,-4),(0,-9)],[(8,-3),(2,5),(3,0)]), [(16, 0), (1, 1), (3, -9)])
         with self.assertRaises(ValueError):
             ca.sumaV([(1,0),(0,-1),(1,-1)],[(2,-2),(1,-1)])
 
     def test_inversaV(self):
         self.assertEqual(ca.inversaV([(1,2),(0,-2),(0,-3)]), [(-1, -2), (0, 2), (0, 3)])
         self.assertEqual(ca.inversaV([(1,0),(0,-1),(-3,1)]), [(-1, 0), (0, 1), (3, -1)])
+        self.assertEqual(ca.inversaV([(-5,2),(3,0),(0,-1)]), [(5, -2), (-3, 0), (0, 1)])
 
-    def test_productoV(self):
-        self.assertEqual(ca.productoV((3,2),[(6,3),(0,0),(5,1),(4,0)]),[(12, 21), (0, 0), (13, 13), (12, 8)])
-        self.assertEqual(ca.productoV((8,-2),[(16,2.3),(0,-7),(6,0),(5,-4)]), [(132.6, -13.6), (-14, -56), (48, -12), (32, -42)])
+    def test_productoCV(self):
+        self.assertEqual(ca.productoCV((3,2),[(6,3),(0,0),(5,1),(4,0)]),[(12, 21), (0, 0), (13, 13), (12, 8)])
+        self.assertEqual(ca.productoCV((8,-2),[(16,2.3),(0,-7),(6,0),(5,-4)]), [(132.6, -13.6), (-14, -56), (48, -12), (32, -42)])
+        self.assertEqual(ca.productoCV((-1,1),[(-2,5),(-1,-1),(2,-9)]), [(-3, -7), (2, 0), (7, 11)])
+
+    def test_normaV(self):
+        self.assertEqual(ca.normaV([(2.6,2.7),(3.7,-6.4)]), 8.288546314040842)
+        self.assertEqual(ca.normaV([(4,5),(3,1),(0,-7)]), 10)
+
+    def test_distanciaMM(self):
+        self.assertEqual(ca.distanciaV([(9,-7),(-1,-6)],[(7,-8),(5,-9)]), 7.0710678118654755)
+        self.assertEqual(ca.distanciaV([(2,7),(4,-1),(2,-4)],[(7,8),(2,-8),(1,4)]), 12.0)
+
+    def test_productoInternoVV(self):
+        self.assertEqual(ca.productoInternoVV([(2,-1),(-8,-5),(-2,-6)],[(6,-3),(5,-1),(-6,-2)]), (4, 1))
 
     # Matrices
     def test_sumaM(self):
         self.assertEqual(ca.sumaM([[(1,0),(0,1)],[(0,1),(3,-4)],[(1,0),(-1,-1)]],[[(2,-1),(0,3)],[(0,-2),(5,0)],[(1,0),(0,-1)]]), [[(3, -1), (0, 4)],[(0, -1), (8, -4)], [(2, 0), (-1, -2)]])
+        self.assertEqual(ca.sumaM([[(-8,-3),(-6,-4),(0,-4)],[(-1,8),(6,-10),(8,-5)],[(4,0),(8,5),(-7,-9)]],[[(-7,-2),(-4,-2),(7,7)],[(5,9),(0,3),(6,-5)],[(1,5),(-6,-6),(5,8)]]), [[(-15, -5), (-10, -6), (7, 3)], [(4, 17), (6, -7), (14, -10)], [(5, 5), (2, -1), (-2, -1)]])
         with self.assertRaises(ValueError):
             ca.sumaM([[(0,1),(3,-4)],[(1,0),(-1,-1)]],[[(2,-1),(0,3)],[(0,-2),(5,0)],[(1,0),(0,-1)]])
 
     def test_inversaM(self):
         self.assertEqual(ca.inversaM([[(1,-1),(0,1),(3,0)],[(5,-4),(6,7),(1,-1)],[(-1,0),(0,-1),(0,9)]]), [[(-1, 1), (0, -1), (-3, 0)], [(-5, 4), (-6, -7), (-1, 1)], [(1, 0), (0, 1), (0, -9)]])
         self.assertEqual(ca.inversaM([[(-3,1),(4,1)], [(-2,1),(2,1)],[(0,-1),(3,1)]]), [[(3, -1), (-4, -1)], [(2, -1), (-2, -1)], [(0, 1), (-3, -1)]])
+        self.assertEqual(ca.inversaM([[(7,3),(-1,7)], [(-9,-4),(-7,-9)]]), [[(-7,-3),(1,-7)], [(9,4),(7,9)]])
 
-    def test_productoM(self):
-        self.assertEqual(ca.productoM((1,-1),[[(0,1),(-1,1)],[(2,1),(-1,2)]]), [[(1, 1), (0, 2)], [(3, -1), (1, 3)]])
-        self.assertEqual(ca.productoM((3,0),[[(1,-2),(-1,1)],[(-2,1),(-1,2)]]), [[(3, -6), (-3, 3)], [(-6, 3), (-3, 6)]])
+    def test_productoCM(self):
+        self.assertEqual(ca.productoCM((1,-1),[[(0,1),(-1,1)],[(2,1),(-1,2)]]), [[(1, 1), (0, 2)], [(3, -1), (1, 3)]])
+        self.assertEqual(ca.productoCM((3,0),[[(1,-2),(-1,1)],[(-2,1),(-1,2)]]), [[(3, -6), (-3, 3)], [(-6, 3), (-3, 6)]])
+        self.assertEqual(ca.productoCM((-2,3),[[(3,-2),(8,-4)],[(4,-10),(-2,-8)]]), [[(0,13), (-4, 32)], [(22, 32), (28, 10)]])
 
     def test_traspuestaM(self):
         self.assertEqual(ca.traspuestaM([[(6,-3),(2,12),(0,-19)], [(0,0),(5,2.1),(17,0)], [(1,0),(2,5),(3,-4.5)]]), [[(6, -3), (0, 0), (1, 0)], [(2, 12), (5, 2.1), (2, 5)], [(0, -19), (17, 0), (3, -4.5)]])
         self.assertEqual(ca.traspuestaM([[1,2],[3,4]]), [[1, 3], [2, 4]])
+        self.assertEqual(ca.traspuestaM([[(5,9),(-7,-5),(-1,-4)], [(8,2),(-3,-7),(7,-8)]]), [[(5, 9), (8, 2)], [(-7, -5), (-3, -7)], [(-1, -4), (7, -8)]])
 
     def test_conjugadaM(self):
         self.assertEqual(ca.conjugadaM([[(6,-3),(2,12),(0,-19)], [(0,0),(5,2.1),(17,0)], [(1,0),(2,5),(3,-4.5)]]), [[(6, 3), (2, -12), (0, 19)], [(0, 0), (5, -2.1), (17, 0)], [(1, 0), (2, -5), (3, 4.5)]])
         self.assertEqual(ca.conjugadaM([[(1,2),(-3,5)],[(3,4),(-1,-1)]]), [[(1, -2), (-3, -5)], [(3, -4), (-1, 1)]])
+        self.assertEqual(ca.conjugadaM([[(-6,1),(3,8)],[(2,-6),(3,0)]]), [[(-6, -1), (3, -8)], [(2, 6), (3, 0)]])
 
     def test_adjuntaM(self):
         self.assertEqual(ca.adjuntaM([[(6,-3),(2,12),(0,-19)], [(0,0),(5,2.1),(17,0)], [(1,0),(2,5),(3,-4.5)]]), [[(6, 3), (0, 0), (1, 0)], [(2, -12), (5, -2.1), (2, -5)], [(0, 19), (17, 0), (3, 4.5)]])
+        self.assertEqual(ca.adjuntaM([[(7,7),(3,8),(8,4)], [(5,0),(8,-6),(-10,-1)]]), [[(7, -7), (5, 0)], [(3, -8), (8, 6)], [(8, -4), (-10, 1)]])
+
+    def test_isUnitaria(self):
+        self.assertEqual(ca.isUnitaria([[(1,0),(0,0)],[(0,0),(1,0)]]), True)
+        self.assertEqual(ca.isUnitaria([[(0,1),(1,0),(0,0)],[(0,0),(0,1),(1,0)],[(1,0),(0,0),(0,1)]]), False)
+        self.assertEqual(ca.isUnitaria([[(1/math.sqrt(2),0),(0,1/math.sqrt(2))],[(0,1/math.sqrt(2)),(1/math.sqrt(2),0)]]), True)
+
+    def test_isHermitiana(self):
+        self.assertEqual(ca.isHermitiana([[(1,0),(3,-1)],[(3,1),(0,1)]]), False)
+        self.assertEqual(ca.isHermitiana([[(3,0),(2,-1),(0,-3)],[(2,1),(0,0),(1,-1)],[(0,3),(1,1),(0,0)]]), True)
+
+    def test_productoMM(self):
+        self.assertEqual(ca.productoMM([[(-6,2),(0,6),(7,2)],[(6,9),(7,7),(-6,-6)],[(5,8),(-6,8),(6,9)]], [[(9,-6),(-3,-4),(5,-2)],[(3,6),(-1,-5),(0,-5)],[(9,9),(8,-4),(-8,-4)]]), [[(-33, 153), (120, 0), (-44, -22)], [(87, 0), (-26, -117), (107, 70)], [(0, 165), (147, 26), (69, -36)]])
+        with self.assertRaises(ValueError):
+            ca.productoMM([[(2,1),(3,0),(1,-1)],[(0,0),(0,-2),(7,-3)],[(3,0),(0,0),(1,-2)]],[[(0,-1),(1,0)],[(0,0),(0,1)]])
+
+    def test_accionMV(self):
+        self.assertEqual(ca.accionMV([[(0,0),(0,-2)],[(0,2),(0,0)]],[(0,1),(1,0)]), [(0, -2), (-2, 0)])
+        self.assertEqual(ca.accionMV([[(-1,5),(1,-7),(-6,3)],[(-3,-9),(2,-5),(1,-10)],[(-6,5),(6,-5),(3,-2)]],[(1,-3),(4,3),(-3,1)]), [(54, -32), (0, 17), (41, 30)])
 
 if __name__ == '__main__':
     unittest.main()
